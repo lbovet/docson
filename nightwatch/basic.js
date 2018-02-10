c98c74723d14afa8b56be9fe2bde1bd8f11b493c
Yanick Champoux <yanick@babyl.dyndns.org>
2018-02-10 13:53:29 -0500

    add test

--- nightwatch/basic.js
+++ nightwatch/basic.js
@@ -3,12 +3,13 @@ let static_app = require( '../lib/server' );
 let server;
 
 const rootUrl = "http://localhost:3000/index.html";
-  n
+  
 module.exports = {
   before: done => {
       server = static_app.listen( 3000, done );
   },
   after: done => server.close( done ),
+
   'relative paths' : function (browser) {
     browser.url( rootUrl + "#/nightwatch/schemas/relative.json").pause(1000);
 
@@ -19,5 +20,18 @@ module.exports = {
       .present;
 
     browser.end();
-  }
+  },
+
+  'resolve #definitions in non-root schema': function(browser) {
+    browser.url( rootUrl + "#/nightwatch/schemas/def-non-root/User.json").pause(1000);
+
+    browser
+      .useXpath()
+      .expect
+      .element(c2x('.property-name:contains("oderOfFirstLastName")'))
+      .present;
+
+    browser.end();
+  },
+
 };
